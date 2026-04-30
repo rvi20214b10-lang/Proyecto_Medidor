34873014939fb005d3f8a63f11c163246d632b53
rvi20214b10-lang <rvi.2021.4b.10@gmail.com>
2026-04-30 18:26:24 +0200

    Use environment variable for server port
    Updated server to use environment variable for port.

--- backend/codpython.py
+++ backend/codpython.py
@@ -1,6 +1,6 @@
 import eventlet
 eventlet.monkey_patch()
-
+import os
 import json
 import sqlite3
 import paho.mqtt.client as mqtt
@@ -134,4 +134,5 @@ client.loop_start()
 # 🚀 RUN SERVER
 # =========================
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+socketio.run(app, host="0.0.0.0", port=port)
